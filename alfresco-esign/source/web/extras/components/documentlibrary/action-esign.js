af438c9598f0333a9ca46f546cdb01a16b457efe
thinhnk55:git config --global user.name thinhnk55git config --global user.name thinhnk55 <khanhthinh.45a4@gmail.com>
2013-09-16 04:19:01 +0700

    refactor

--- alfresco-esign/source/web/extras/components/documentlibrary/action-esign.js
+++ alfresco-esign/source/web/extras/components/documentlibrary/action-esign.js
@@ -1,4 +1,26 @@
-/**
+
+/****************************************************************************** * 
+E-Learning GateIn System is e-learning system based on GateIn Portal.
+* Copyright (C) 2013   Nguyen Khanh Thinh, Nguyen Dinh Nien Contributors
+* This program is free software; you can redistribute it and/or 
+* modify it under the terms of the GNU General Public License 
+* as published by the Free Software Foundation; either version 2 
+* of the License, or (at your option) any later version. 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+* GNU General Public License for more details.  
+* You should have received a copy of the GNU General Public License 
+* along with this program; if not, write to the Free Software 
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+* Any further request, feel freely to contactmhst1304@googlegroups.com or 
+* visit https://www.hostedredmine.com/projects/mhst1304 or
+* participate online daily meeting via IRC channel #mhst1304 server freenode.net at 9PM - GMT+7.
+
+*************************************************************************************************/
+
+
+/** 
  * Document library Execute Script action
  * 
  * @namespace Alfresco
